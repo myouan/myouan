ef275d77c7ed41ad11bfe511482010e7e3614092
Takashi Kitajima <inc@2inc.org>
2016-06-09 09:54:18 +0900

    Update footer style

--- footer.php
+++ footer.php
@@ -1,20 +1,22 @@
 	</div>
 
-	<?php if ( is_active_sidebar( 'footer' ) ) : ?>
-	<div class="widgets-footer">
-		<div class="container">
-			<div class="row">
-				<?php dynamic_sidebar( 'footer' ); ?>
+	<footer id="footer" class="l-footer">
+		<?php if ( is_active_sidebar( 'footer' ) ) : ?>
+		<div class="widgets-footer">
+			<div class="container">
+				<div class="row">
+					<?php dynamic_sidebar( 'footer' ); ?>
+				</div>
 			</div>
 		</div>
-	</div>
-	<?php endif; ?>
+		<?php endif; ?>
 
-	<div class="p-copyright">
-		<div class="container">
-			Copyright <?php bloginfo( 'name' ); ?> All rights reserved.
+		<div class="p-copyright">
+			<div class="container">
+				Copyright <?php bloginfo( 'name' ); ?> All rights reserved.
+			</div>
 		</div>
-	</div>
+	</footer>
 
 	<?php wp_footer(); ?>
 </body>

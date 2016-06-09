9283948d5bb1f213ab37e7ae4e9e35eedb5d4fe5
Takashi Kitajima <inc@2inc.org>
2016-06-09 11:00:14 +0900

    Added front-page-2 template

---
+++ page-templates/front-page-2.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Template Name: Front Page 2
+ */
+?>
+<?php get_header(); ?>
+
+<?php get_template_part( 'templates/object/project/main-visual' ); ?>
+<?php get_template_part( 'templates/object/project/front-page-widget-area--top' ); ?>
+<?php get_template_part( 'templates/object/project/3points' ); ?>
+<?php get_template_part( 'templates/object/project/gallery' ); ?>
+<?php get_template_part( 'templates/object/project/recent-posts-list' ); ?>
+<?php get_template_part( 'templates/object/project/front-page-widget-area--bottom' ); ?>
+<?php get_template_part( 'templates/object/project/social-nav' ); ?>
+
+<?php get_footer(); ?>

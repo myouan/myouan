b376a96126ca1dcacba70c1c788f4b22819f999e
Takashi Kitajima <inc@2inc.org>
2018-03-24 16:34:55 +0900

    p-widget to c-widget

--- functions/register-sidebars.php
+++ functions/register-sidebars.php
@@ -4,9 +4,9 @@ function wpbook_widgets_init() {
 		'name'          => __( 'サイドバー', 'wpbook' ),
 		'id'            => 'sidebar',
 		'description'   => __( 'サイドバー', 'wpbook' ),
-		'before_widget' => '<section id="%1$s" class="p-widget p-widget--sidebar %2$s">',
+		'before_widget' => '<section id="%1$s" class="c-widget %2$s">',
 		'after_widget'  => '</section>',
-		'before_title'  => '<h1 class="p-widget__title">',
+		'before_title'  => '<h1 class="c-widget__title">',
 		'after_title'   => '</h1>',
 	) );
 
@@ -14,9 +14,9 @@ function wpbook_widgets_init() {
 		'name'          => __( 'フッター', 'wpbook' ),
 		'id'            => 'footer',
 		'description'   => __( 'フッター', 'wpbook' ),
-		'before_widget' => '<section id="%1$s" class="p-widget p-widget--footer col-md-4 %2$s">',
+		'before_widget' => '<section id="%1$s" class="c-widget col-md-4 %2$s">',
 		'after_widget'  => '</section>',
-		'before_title'  => '<h1 class="p-widget__title">',
+		'before_title'  => '<h1 class="c-widget__title">',
 		'after_title'   => '</h1>',
 	) );
 
@@ -24,9 +24,9 @@ function wpbook_widgets_init() {
 		'name'          => __( 'メインビジュアル', 'wpbook' ),
 		'id'            => 'main-visual',
 		'description'   => __( 'メインビジュアル', 'wpbook' ),
-		'before_widget' => '<section id="%1$s" class="p-widget p-widget--main-visual %2$s">',
+		'before_widget' => '<section id="%1$s" class="c-widget %2$s">',
 		'after_widget'  => '</section>',
-		'before_title'  => '<h1 class="p-widget__title">',
+		'before_title'  => '<h1 class="c-widget__title">',
 		'after_title'   => '</h1>',
 	) );
 
@@ -34,9 +34,9 @@ function wpbook_widgets_init() {
 		'name'          => __( 'トップページ上部', 'wpbook' ),
 		'id'            => 'front-page-top',
 		'description'   => __( 'トップページ上部', 'wpbook' ),
-		'before_widget' => '<section id="%1$s" class="p-widget p-widget--front-page-top %2$s">',
+		'before_widget' => '<section id="%1$s" class="c-widget %2$s">',
 		'after_widget'  => '</section>',
-		'before_title'  => '<h1 class="p-widget__title">',
+		'before_title'  => '<h1 class="c-widget__title">',
 		'after_title'   => '</h1>',
 	) );
 
@@ -44,9 +44,9 @@ function wpbook_widgets_init() {
 		'name'          => __( 'トップページ下部', 'wpbook' ),
 		'id'            => 'front-page-bottom',
 		'description'   => __( 'トップページ下部', 'wpbook' ),
-		'before_widget' => '<section id="%1$s" class="p-widget p-widget--front-page-bottom %2$s">',
+		'before_widget' => '<section id="%1$s" class="c-widget %2$s">',
 		'after_widget'  => '</section>',
-		'before_title'  => '<h1 class="p-widget__title">',
+		'before_title'  => '<h1 class="c-widget__title">',
 		'after_title'   => '</h1>',
 	) );
 
@@ -54,9 +54,9 @@ function wpbook_widgets_init() {
 		'name'          => __( '投稿・お知らせ記事下', 'wpbook' ),
 		'id'            => 'post-bottom',
 		'description'   => __( '投稿・お知らせ記事下', 'wpbook' ),
-		'before_widget' => '<section id="%1$s" class="p-widget p-widget--post-bottom %2$s">',
+		'before_widget' => '<section id="%1$s" class="c-widget %2$s">',
 		'after_widget'  => '</section>',
-		'before_title'  => '<h1 class="p-widget__title">',
+		'before_title'  => '<h1 class="c-widget__title">',
 		'after_title'   => '</h1>',
 	) );
 
@@ -64,9 +64,9 @@ function wpbook_widgets_init() {
 		'name'          => __( '投稿・お知らせ記事タイトル下', 'wpbook' ),
 		'id'            => 'post-title-bottom',
 		'description'   => __( '投稿・お知らせ記事タイトル下', 'wpbook' ),
-		'before_widget' => '<section id="%1$s" class="p-widget p-widget-post-title-bottom %2$s">',
+		'before_widget' => '<section id="%1$s" class="c-widget %2$s">',
 		'after_widget'  => '</section>',
-		'before_title'  => '<h1 class="p-widget__title">',
+		'before_title'  => '<h1 class="c-widget__title">',
 		'after_title'   => '</h1>',
 	) );
 }
